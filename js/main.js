63ad6a093f678f12e51ff592d2c738a9a233f482
Luigi Fontana <luigifontana92@gmail.com>
2020-02-25 16:32:51 +0100

    Chiusura esercizio

--- js/main.js
+++ js/main.js
@@ -8,7 +8,11 @@
     BONUS: all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali.
     Con difficoltà 0=> tra 1 e 100, con difficoltà 1 =>  tra 1 e 80, con difficoltà 2=> tra 1 e 50
 */
+
+// Riempirò questo codice di commenti :P
+
 var minePiazzate = [] // array vuoto dove verranno pushate le mine, i numeri estratti random dal pc
+var bandierinePiazzate = []; // array vuoto dove verranno pushate le bandierine, i numeri inseriti dall'utente
 
 for (var i = 0; minePiazzate.length < 16; i++) { // Ciclo FOR fino a raggiungere 16 elementi nell'array minePiazzate
     var minaDaPiazzare = generaRandomMinMax(1, 100); // Generazione mina, numero random tramite funzione
@@ -17,9 +21,35 @@ for (var i = 0; minePiazzate.length < 16; i++) { // Ciclo FOR fino a raggiungere
     }
 }
 
-console.log(minePiazzate);
-
-
+for (var i = 0; bandierinePiazzate.length < 85; i++) { // Ciclo FOR fino a raggiungere le 84 bandierine massime piazzabili (caso improbabilissimo)
+    var bandierinaDaPiazzare = parseInt(prompt('Scrivi un numero da 1 a 100 e spera di non esplodere!'));
+    if (!isNaN(bandierinaDaPiazzare)) { // se è un numero vai avanti, altrimenti salta alla riga49
+        if ((bandierinaDaPiazzare >= 1) && (bandierinaDaPiazzare <= 100)) { // se è compreso tra 1 e 100 vai avanti, altrimenti salta alla riga46
+            if (!bandierinePiazzate.includes(bandierinaDaPiazzare)) { // se il numero non fa parte dei numeri già provati vai avanti, altrimenti salta alla riga43
+                if (!minePiazzate.includes(bandierinaDaPiazzare)) { // se il numero non fa parte dei numeri dell'array minePiazzate vai avanti, altrimenti salta alla riga32
+                    bandierinePiazzate.push(bandierinaDaPiazzare) // inserisci il numero nell'array bandierinePiazzate
+                    alert('FIUUU! Bandierina Piazzata!');
+                } else { // rif. IF riga29
+                    if (bandierinePiazzate.length === 1) { // caso in cui la lunghezza dell'array bandierinePiazzate è 1
+                        alert('|*|* BOOOM *|*| Game Over! Hai piazzato una sola bandierina');
+                    } else if (bandierinePiazzate.length === 84){ // caso in cui la lunghezza dell'array bandierinePiazzate è il massimo 84
+                        alert('Sei un mostro! Hai piazzato tutte le bandierine! Vai subito a comprare un gratta e vinci :)');
+                    } else { // casi in cui la lunghezza dell'array bandierinePiazzate è tra 2 e 83
+                        alert('|*|* BOOOM *|*| Game Over! Hai piazzato ' + bandierinePiazzate.length + ' bandierine.');
+                    }
+                    alert('Ricarica la pagina per una nuova partita');
+                    break; // una volta arrivati qui il ciclo va bloccato altrimenti stai barando :P
+                }
+            } else { // rif. IF riga28
+                alert('Hai già piazzato una bandierina qui furbetto');
+            }
+        } else { // rif. IF riga27
+            alert('Devi inserire un numero da 1 a 100');
+        }
+    } else { // rif. IF riga26
+        alert('Perfavore inserisci un numero');
+    }
+}
 
 function generaRandomMinMax(min, max) { // funzione che genera un numero random tra due valori dati in ingresso MIN e MAX, estremi inclusi
     var numeroRandom = Math.floor(Math.random() * (max - min + 1)) + min;
